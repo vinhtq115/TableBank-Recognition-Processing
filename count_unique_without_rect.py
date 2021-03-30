46eaa690911bbef79eab26a2d28619b55b4da4f7
Vinh Quang Tran <starcraft6723@hotmail.com>
2021-03-30 22:14:42 +0700

    Time to graduate?

--- count_unique_without_rect.py
+++ count_unique_without_rect.py
@@ -13,6 +13,10 @@ for root, dirs, files in os.walk(r'E:\TableBank-Recognition\Recognition\flows\fl
     for file in files:
         detected_files.add(file)
 
+for root, dirs, files in os.walk(r'E:\TableBank-Recognition\Recognition\flows\flow4'):
+    for file in files:
+        detected_files.add(file)
+
 with open(r'E:\TableBank-Recognition\Recognition\flows\List of files with missing font (rectangles).txt') as f:
     wrong_list = f.readlines()
     wrong_list = [line.strip().replace('.png', '.xml') for line in wrong_list]
